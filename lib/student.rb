3bdf9b822375d9eb457d496507bede66e0b77eac
Ryan Mui <reachryanmui@gmail.com>
2020-03-10 02:17:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,6 +40,9 @@ class Student
 
   def self.all_students_in_grade_9
     sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
     SQL
 
     DB[:conn].execute(sql).map do |row|
